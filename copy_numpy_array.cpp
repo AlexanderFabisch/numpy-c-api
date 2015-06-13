0f4bda6d8ff3b57c06da7dab47361e47e6242438
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2015-06-13 20:17:33 +0200

    C++11 style

--- copy_numpy_array.cpp
+++ copy_numpy_array.cpp
@@ -6,14 +6,11 @@
 int main()
 {
     const PythonInterpreter& python = PythonInterpreter::instance();
-    std::vector<double> array;
-    array.push_back(1.2);
-    array.push_back(2.3);
-    array.push_back(3.4);
+    std::vector<double> array{1.2, 2.3, 3.4};
     python.callFunction("functions", "take_array", array);
     std::vector<double> result = python.callReturnFunction(
         "functions", "produce_array");
-    for(auto r = result.begin(); r != result.end(); r++)
-        std::cout << *r << ", ";
+    for(double& r: result)
+        std::cout << r << ", ";
     std::cout << std::endl;
 }
